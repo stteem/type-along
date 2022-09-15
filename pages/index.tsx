52f6d4ade3698ef757018e45da4b3215428cbe39
Uwem Uke <slyart147@gmail.com>
2022-09-15 15:54:56 +0100

    button wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,17 +62,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-        {' '}Copyright &copy; 2022
+        Copyright &copy; 2022
         <a id={styles.name}
         href="https://www.linkedin.com/in/uwem-uke-9a6b06125/" 
         target="_blank"
